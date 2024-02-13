4c34fe6d30a53a38ea440a483e5aac88f34b5639
Ajayi AKINSOLA MARTINS <145346225+martinsAkin@users.noreply.github.com>
2024-02-13 23:09:20 +0100

    Add files via upload
    A landing page for a Sunnyside group of companies

---
+++ Sunnyside.js
@@ -0,0 +1,16 @@
+/* const bar = document.getElementById('bar');
+function active() {
+    const bar = document.getElementById('menubar');
+    if(bar.style.display === 'none') {
+        bar.style.display == 'block';
+    } */
+
+function active(){
+         let bar = document.getElementById("aside");
+          if(bar.style.display === "none") {
+              bar.style.display ="block";
+          }else {
+              bar.style.display ="none";
+          }
+          
+      }     
